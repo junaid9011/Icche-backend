77ee5877bdf14762783f776e47a2c68daf654164
xunaid5 <junaid.cou@gmail.com>
2022-08-06 14:48:18 +0600

    img-upload

--- app.js
+++ app.js
@@ -1,49 +1,55 @@
-const express=require('express');
-const cors=require('cors');
-const app=express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
 // const corsOptions ={
 //     origin:'http://localhost:3000/', 
 //     credentials:true,            //access-control-allow-credentials:true
 //     optionSuccessStatus:200
 // }
 app.use(cors());
-const errorMiddleware=require('./Middleware/Error')
+const errorMiddleware = require('./Middleware/Error')
 
-const bodyParser=require('body-parser')
-const dotenv=require('dotenv');
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 // const cloudinary=require('cloudinary')
 // const fileUpload =require('express-fileupload')
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json({
+    limit: '50mb'
+  }));
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    parameterLimit: 100000,
+    extended: true
+}))
 app.use(cookieParser());
 
 
 // app.use(fileUpload());
 
 
-dotenv.config({path:'F:\\Icche\\config\\config.env'}); 
+dotenv.config({ path: 'F:\\Icche\\config\\config.env' });
 
 
 //importes all routes
-const productRoute=require('./Routes/Product');
-const authRoute=require('./Routes/auth');
-const orderRoute=require('./Routes/orderRoute');
+const productRoute = require('./Routes/Product');
+const authRoute = require('./Routes/auth');
+const orderRoute = require('./Routes/orderRoute');
 
 
-app.use('/api/v1',productRoute);
-app.use('/api/v1',authRoute);
-app.use('/api/v1',orderRoute);
+app.use('/api/v1', productRoute);
+app.use('/api/v1', authRoute);
+app.use('/api/v1', orderRoute);
 
 
-    // app.use(express.static(path.join(__dirname,'../front-end',)))
-    // app.get('*',(req,res)=>{
-    // res.sendFile(path.resolve(__dirname, '../front-end/build/index.html'))
+// app.use(express.static(path.join(__dirname,'../front-end',)))
+// app.get('*',(req,res)=>{
+// res.sendFile(path.resolve(__dirname, '../front-end/build/index.html'))
 
-    // })
+// })
 
 //Middleware to handle erros
 app.use(errorMiddleware)
 
-module.exports=app;
+module.exports = app;
